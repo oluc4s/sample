019754c73f135c0e031cb637ffd76a7567137e1c
oluca <matheuslucas.ti@gmail.com>
2024-05-17 20:02:36 -0300

    Initial commit

---
+++ common/src/main/java/com/s2start/common/uikit/StartView.kt
@@ -0,0 +1,10 @@
+package com.s2start.common.uikit
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun StartView() {
+    Column{ Text("StartView Lib") }
+}
